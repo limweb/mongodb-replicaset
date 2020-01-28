f3125f1c14a7af329be821679ce0e81f330923ac
lim web <limweb@hotmail.com>
2020-01-28 17:19:18 +0700

    .

--- setup/replicaSet.js
+++ setup/replicaSet.js
@@ -1,7 +1,7 @@
 rsconf = {
   _id : "rs0",
   members: [
-    { _id : 0, host : "mongo-rs0-1:27017" , priority : 1},
+    { _id : 0, host : "mongo-rs0-1:27017" , priority : 2},
     { _id : 1, host : "mongo-rs0-2:27017" },
     { _id : 2, host : "mongo-rs0-3:27017" }
   ]
